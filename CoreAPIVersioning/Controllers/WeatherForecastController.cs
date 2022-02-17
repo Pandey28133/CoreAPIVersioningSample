73c46b7b91c45d5432026543cfd0d9252e1de643
Ganesh Pandey <gpandey@markem-imaje.com>
2022-02-17 23:38:11 +0530

    All type of version covered URL String parameter Accept header custom header

--- CoreAPIVersioning/Controllers/WeatherForecastController.cs
+++ CoreAPIVersioning/Controllers/WeatherForecastController.cs
@@ -10,7 +10,19 @@ namespace CoreAPIVersioning.Controllers
     
     [ApiController]
 
-    [Route("[controller]")]
+    [Route("api/[Controller]")]
+
+    //[Route("api/v{version:apiVersion}/[Controller]")]
+    //example https://localhost:44363/api/v1.0/weatherforecast/getreport
+    //example https://localhost:44363/api/v2.0/weatherforecast/getreport
+    //example https://localhost:44363/api/v1/weatherforecast/getreport
+    //example https://localhost:44363/api/v2/weatherforecast/getreport
+
+    //[ApiVersion("1.0")]
+
+    // response header api-deprecated-versions:1.0: to let the clinet know about the versions of API is Deprecated
+    [ApiVersion("1.0", Deprecated =true)]
+    [ApiVersion("2.0")]
     
     public class WeatherForecastController : ControllerBase
     {
@@ -26,9 +38,9 @@ namespace CoreAPIVersioning.Controllers
             _logger = logger;
         }
 
-        
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        // examplae https://localhost:44363/api/weatherforecast/getreport
+        [HttpGet("getreport")]        
+        public IEnumerable<WeatherForecast> GetWeatherForecast()
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -36,6 +48,24 @@ namespace CoreAPIVersioning.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        // examplae https://localhost:44363/api/weatherforecast/getreport?api-version=2.0    -- by default query string parameter version
+        [HttpGet("getreport")]
+        [MapToApiVersion("2.0")]
+        public IEnumerable<WeatherForecastV1> GetWeatherForecastV1()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecastV1
+            {
+                ApiVersion = "Done by Pandey",
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+                
+                
             })
             .ToArray();
         }
--- CoreAPIVersioning/Startup.cs
+++ CoreAPIVersioning/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +27,31 @@ namespace CoreAPIVersioning
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddApiVersioning();
+            services.AddApiVersioning(x=> {
+                x.AssumeDefaultVersionWhenUnspecified = true;
+                // default Mediatype Accept pass "version" from request header and this will overridde default url API versioning
+                // for example:-   accept = application/json; version=2.0
+
+                //x.ApiVersionReader = new MediaTypeApiVersionReader("version");
+
+                // custome header name for api version, pass "x-version" from request header and this will overridde default url API versioning
+                // for example;-  x-version = 2.0
+
+                x.ApiVersionReader = new HeaderApiVersionReader("x-version");
+
+                // to support Mediatype Accept and custom header name 
+                // default Mediatype Accept pass "version" from request header and this will overridde default url API versioning
+                // custome header name for api version, pass "x-version" from request header and this will overridde default url API versioning
+                // for example:-   accept = application/json; version=2.0 or x-version = 2.0                 
+
+                //x.ApiVersionReader = new ApiVersionReader.Combine(
+                //    new MediaTypeApiVersionReader("version"),
+                //    new HeaderApiVersionReader("x-version")
+                //    );                
+
+                // response header api-supported-versions: 1.0, 2.0 to let the clinet know about the versions of API avaialbity
+                x.ReportApiVersions = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
